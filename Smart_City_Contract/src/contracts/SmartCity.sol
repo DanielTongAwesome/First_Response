6045e8859bc74d625f9b1721af1a1b7d9cb2bb2f
DanielTongAwesome <danieltongubc@gmail.com>
2020-03-28 18:23:09 -0700

    [#2] add intersection warning function, public warning

--- Smart_City_Contract/src/contracts/SmartCity.sol
+++ Smart_City_Contract/src/contracts/SmartCity.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract SmartCity {
     string public appName;
     uint public eventNumber = 0; // total number of detected event
+    uint public publicWarnNumber = 0;
     // event status to keep track of the status of reward event
     /*
     enum explaination
@@ -24,9 +25,19 @@ contract SmartCity {
         warningFlag status;     // warning flag status
     }
 
+    struct PublicWarnEvent {
+        uint256 eventID;    // tract event number
+        uint256 timestamp;  // timestamp
+        string  roadName1;
+        string  roadName2;
+        uint256 warningCode;    // warning code
+        warningFlag status;     // warning flag status
+    }
+
     // mapping adress to BroadcastEvent - type public
     // use uint256 => BroadcastEvent because one user can send multiple events
     mapping(uint256 => BroadcastEvent) public BroadcastEvents;
+    mapping(uint256 => PublicWarnEvent) public PublicWarnEvents;
 
     // contract constructor
     constructor() public {
@@ -48,24 +59,29 @@ contract SmartCity {
         uint256 objectYCor,     // object y corodinates
         uint256 mode,           // form of transportation
         warningFlag status      // warning flag status
-    ); 
+    );
 
     // broadcast warning event
-    event broadcastIntersectionWarningEvent(
+    event broadcastPublicWarningEvent(
         uint256 eventID,    // tract event number
         uint256 timestamp,  // timestamp
-        address intersectionZip,        // example: V6T 1Z4
-        address intersectionLocation,   // example: UBC Bus Loop
+        string roadName1,        // example: V6T 1Z4
+        string roadName2,   // example: UBC Bus Loop
         warningFlag status      // warning flag status
-    ); 
+    );
 
     event call911Event(
         uint256 eventID,    // tract event number
         uint256 timestamp,  // timestamp
-        address intersectionZip,        // example: V6T 1Z4
-        address intersectionLocation,   // example: UBC Bus Loop
+        string intersectionZip,        // example: V6T 1Z4
+        string intersectionLocation,   // example: UBC Bus Loop
         warningFlag status      // warning flag status
     );
+
+
+
+
+
 
     // function declearation
     // broadcast the warning msg to the network
@@ -77,11 +93,11 @@ contract SmartCity {
         uint256 _mode,           // form of transportation
         uint256 _warningCode     // warning code
     ) public {
-        require(_timestamp  != 0, "Timestamp not equal to 0");
-        require(_objectID   != 0, "Object ID not equal to 0");
+        require(_timestamp != 0, "Timestamp not equal to 0");
+        require(_objectID != 0, "Object ID not equal to 0");
         require(_objectXCor != 0, "Object X cordinates not equal to 0");
         require(_objectYCor != 0, "Object Y cordinates not equal to 0");
-        require(_mode       != 0, "mode not equal to 0");
+        require(_mode != 0, "mode not equal to 0");
 
         // TODO: only admin can call this function
 
@@ -109,6 +125,43 @@ contract SmartCity {
             BroadcastEvents[eventNumber].objectYCor,    // object y corodinates
             BroadcastEvents[eventNumber].mode,          // form of transportation
             BroadcastEvents[eventNumber].status        // warning flag status
+        );
+    }
+
+
+    // function declearation
+    // broadcast the warning msg to the network
+    function broadcastIntersectionWarning(
+        uint256 _timestamp,  // timestamp
+        uint256 _warningCode,     // warning code
+        string memory _roadName1,    // road intersection 1
+        string memory _roadName2    // road intersection 2
+    ) public {
+        require(_timestamp != 0, "Timestamp not equal to 0");
+        require(bytes(_roadName1).length > 0, "Road name should not be empty");
+        require(bytes(_roadName2).length > 0, "Road name should not be empty");
+        // TODO: only admin can call this function
+
+        // accumulate num
+        publicWarnNumber++;
+
+        // save info
+        PublicWarnEvents[publicWarnNumber] = PublicWarnEvent(
+            publicWarnNumber,            // tract event number
+            _timestamp,
+            _roadName1,
+            _roadName2,
+            _warningCode,
+            warningFlag(_warningCode)     // warning flag status
+        );
+
+        // broadcast to the network
+        emit broadcastPublicWarningEvent(
+            PublicWarnEvents[publicWarnNumber].eventID,       // tract event number
+            PublicWarnEvents[publicWarnNumber].timestamp,     // timestamp
+            PublicWarnEvents[publicWarnNumber].roadName1,
+            PublicWarnEvents[publicWarnNumber].roadName2,
+            PublicWarnEvents[publicWarnNumber].status        // warning flag status
         );
     }
 
--- Smart_City_Contract/test/SmartCity.test.js
+++ Smart_City_Contract/test/SmartCity.test.js
@@ -23,7 +23,7 @@ contract(SmartCity,([deployer, seller, buyer])=>{
         })
     })
 
-     // create issue tests
+     // Testing Broadcast Warning
      describe('Testing Broadcast Warning', async () => {
         let result;
         let numRequests;
@@ -37,7 +37,7 @@ contract(SmartCity,([deployer, seller, buyer])=>{
         it('System should create a broadcast successfully', async () => {
             assert.equal(numRequests, 1);
             const event = result.logs[0].args;
-            console.log(event)
+            //console.log(event)
             assert.equal(event.timestamp.toNumber(),    123, 'number of timestamp should be 123');
             assert.equal(event.objectID.toNumber(),     1,   'objectID should be 1');
             assert.equal(event.objectXCor.toNumber(),   123, 'objectXCor should be 123');
@@ -46,7 +46,44 @@ contract(SmartCity,([deployer, seller, buyer])=>{
             assert.equal(event.status,                  0,   'status should be 0')
         })
 
+        // a request should be rejected
+        it('Failure cases tests ', async () => {
+            await smartCity.broadcastWarning('','1','123', '123', '1', '0',{ from: deployer }).should.be.rejected;
+            await smartCity.broadcastWarning('123','','123', '123', '1', '0',{ from: deployer }).should.be.rejected;
+        })
+
     })
+
+
+     // Test Broadcast Intersection Warning
+     describe('Test Broadcast Intersection Warning', async () => {
+        let result;
+        let numRequests;
+
+        before(async () => {
+            result = await smartCity.broadcastIntersectionWarning('123','2','4th road', '5th road', { from: deployer })
+            numRequests = await smartCity.publicWarnNumber()
+        })
+
+        // create a request
+        it('System should create a broadcast successfully', async () => {
+            assert.equal(numRequests, 1);
+            const event = result.logs[0].args;
+            //console.log(event)
+            assert.equal(event.timestamp.toNumber(),    123, 'number of timestamp should be 123');
+            assert.equal(event.roadName1,   '4th road',   'road 1 should be 4th road');
+            assert.equal(event.roadName2,   '5th road',   'road 2 should be 5th road');
+            assert.equal(event.status,               2,'status should be 2 - potential injuries')
+        })
+
+        // a request should be rejected
+        it('Failure cases tests ', async () => {
+            await smartCity.broadcastIntersectionWarning('','2','4th road', '5th road', { from: deployer }).should.be.rejected;
+            await smartCity.broadcastIntersectionWarning('123','2','', '5th road', { from: deployer }).should.be.rejected;
+        })
+
+    })
+
 
 
     
